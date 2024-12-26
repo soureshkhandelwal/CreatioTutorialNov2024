8f960efcd0e354ec3feb10908cb5100e72ad6572
souresh kumar <soureshkhandelwal53@gmail.com>
2024-12-27 00:22:50 +0530

    Remove Concert Stuff

--- btrns_sk_useCase_concert/Autogenerated/Src/UsrConcertUseCase06650f04Section.btrns_sk_useCase_concert.js
+++
@@ -1,8 +0,0 @@
-﻿define("UsrConcertUseCase06650f04Section", [], function() {
-	return {
-		entitySchemaName: "UsrConcertUseCase",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
-});
